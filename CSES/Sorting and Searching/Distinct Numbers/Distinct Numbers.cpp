cf753a1a31161319d973c246ff7a9f1951be126b
lucasfrancisco <lucasfranciscosp7@gmail.com>
2023-12-27 01:32:31 -0300

    Distinct Numbers

---
+++ CSES/Sorting and Searching/Distinct Numbers/Distinct Numbers.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+#define ou ||
+#define dbg(x) cout << #x << " = " << x << endl
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+ 
+using namespace std;
+ 
+int main(){_
+    int n, m;cin>>n;
+    int saida=0;
+    set<int> est;
+    for(int i=0;i<n;i++){
+        int num;cin>>num;
+        est.insert(num);
+    }
+    cout<<est.size()<<endl;
+    return 0;
+}
